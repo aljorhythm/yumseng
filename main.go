a01b793cd95447e11224489377cbe04994ed4614
Joel Lim <103879u@gmail.com>
2021-11-21 02:31:36 +0800

    edit

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
 )
+
+// content holds our static web server content.
+//go:embed webui/*
+var webuiFs embed.FS
+
+func generateUiHandler() (http.Handler, error) {
+	uiFileSystem, err := fs.Sub(webuiFs, "webui")
+	if err != nil {
+		return nil, err
+	}
+	return http.FileServer(http.FS(uiFileSystem)), nil
+}
 
 func cheersHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Cheers!")
@@ -21,7 +35,15 @@ func getPort() string {
 }
 
 func main() {
+	webuiHandler, err := generateUiHandler()
+
+	if err != nil {
+		log.Panicf("[main.go#main] Error generateUiHandler %s", err)
+	}
+
+	http.Handle("/", webuiHandler)
 	http.HandleFunc("/cheers", cheersHandler)
+
 	port := getPort()
 	log.Printf("Running server PORT=%s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
