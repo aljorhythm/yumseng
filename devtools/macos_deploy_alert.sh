44d1904a2ac5001b957e9413176cd2b4a50e5266
Joel Lim <103879u@gmail.com>
2021-11-30 01:51:44 +0800

    [devops][macos] fix deployment alert

--- devtools/macos_deploy_alert.sh
+++ devtools/macos_deploy_alert.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 while true; do
-  data=$(curl yumseng.herokuapp.com/ping)
+  data=$(curl localhost/ping)
 
   echo "$data"
 
-  if [ "$olddata" != "" ] && [ "$olddata" != $data ]; then
-    osascript -e 'yumseng was deployed"'"$data"'"'
+  tag=$(echo $data | jq ".Tag")
+  echo tag "$tag" oldtag "$oldtag"
+
+  if [ "$oldtag" != "" ] && [ "$oldtag" != "$tag" ]; then
+    echo changed $tag
+    osascript \
+      -e "on run(argv)" \
+      -e "return display alert \"Yumseng was deployed!\" message item 1 of argv" \
+      -e "end" \
+      -- "$tag"
   fi
-  olddata=$data
+
+  oldtag=$tag
+
   sleep 5
 done
