4c9bd1da278941dacb7c38033a697068fe923997
noellim <noellimx@gmail.com>
2021-12-05 20:01:23 +0800

    + a room to display room id and click button

--- react-ts-ui/src/components/Room.tsx
+++ react-ts-ui/src/components/Room.tsx
@@ -1,19 +1,38 @@
+import ButtonSendCheer from "./ButtonSendCheer";
 interface RoomProp {
   name: string | null;
 }
 
 const Room = (prop: RoomProp) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        verticalAlign: "middle",
-        justifyContent: "center",
-        alignSelf: "center",
-      }}
-    >
-      {prop.name}
-    </div>
+    <>
+      <div
+        style={{
+          display: "grid",
+          border: "solid",
+          height: "100%",
+          width: "100%",
+          alignItems: "center",
+          flexFlow: "column wrap",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <div style={{ display: "inherit", verticalAlign: "top", flex: "1" }}>
+          {prop.name}
+        </div>
+
+        <div
+          style={{
+            display: "inherit",
+            height: "100%",
+            verticalAlign: "middle",
+          }}
+        >
+          <ButtonSendCheer></ButtonSendCheer>
+        </div>
+      </div>
+    </>
   );
 };
 
